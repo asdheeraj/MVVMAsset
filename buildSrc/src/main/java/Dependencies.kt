7818464fa72d3e950fcf66d50ab9b043357a0096
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-20 22:30:13 +0530

    Depencies moved to a kotlin file

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,26 @@
+object Versions {
+    const val life_cycle = "2.2.0"
+    const val kotlin_version = "1.4.31"
+    const val core_ktx = "1.3.2"
+    const val app_compat = "1.2.0"
+    const val material = "1.3.0"
+    const val constraint_layout = "2.0.4"
+    const val junit = "4.13.2"
+    const val test_ext = "1.1.2"
+    const val espresso_core = "3.3.0"
+}
+
+object Dependencies {
+    const val lifecycleCommon = "androidx.lifecycle:lifecycle-common:${Versions.life_cycle}"
+    const val lifeCycleRunTime = "androidx.lifecycle:lifecycle-runtime:${Versions.life_cycle}"
+    const val lifeCycleExtensions = "android.arch.lifecycle:extensions:${Versions.life_cycle}"
+    const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.life_cycle}"
+    const val kotlin_library = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin_version}"
+    const val core_ktx = "androidx.core:core-ktx:${Versions.core_ktx}"
+    const val app_compat = "androidx.appcompat:appcompat:${Versions.app_compat}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+    const val constraint_layout = "androidx.constraintlayout:constraintlayout:${Versions.constraint_layout}"
+    const val junit = "junit:junit:${Versions.junit}"
+    const val test_ext = "androidx.test.ext:junit:${Versions.test_ext}"
+    const val espresso_core = "androidx.test.espresso:espresso-core:${Versions.espresso_core}"
+}
