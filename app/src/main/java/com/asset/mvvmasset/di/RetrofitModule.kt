e8ec024eb55b706d93f43fc2513b27d8112d1163
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-21 18:13:01 +0530

    Added the required modules that are requried to "Provide" the dependencies

---
+++ app/src/main/java/com/asset/mvvmasset/di/RetrofitModule.kt
@@ -0,0 +1,35 @@
+package com.asset.mvvmasset.di
+
+import com.asset.mvvmasset.data.api.MoviesApiService
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideGsonBuilder(): Gson = GsonBuilder().create()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson): Retrofit.Builder {
+        return Retrofit.Builder()
+            .baseUrl("https://open-api.xyz/placeholder/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+    }
+
+    @Singleton
+    @Provides
+    fun provideBlogApi(retrofit: Retrofit.Builder): MoviesApiService {
+        return retrofit.build().create(MoviesApiService::class.java)
+    }
+}
