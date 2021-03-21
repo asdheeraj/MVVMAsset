ae067ce366a77b9c83446b555b534681a1b9e130
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2021-03-21 18:59:53 +0530

    Coroutine Dispatcher provided through DI for testability purposes

--- app/src/main/java/com/asset/mvvmasset/presentation/viewmodel/MoviesViewModel.kt
+++ app/src/main/java/com/asset/mvvmasset/presentation/viewmodel/MoviesViewModel.kt
@@ -7,13 +7,14 @@ import com.asset.mvvmasset.data.model.MoviesResponse
 import com.asset.mvvmasset.data.util.Resource
 import com.asset.mvvmasset.domain.usecase.GetPopularMoviesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MoviesViewModel @Inject constructor(
-    private val getPopularMoviesUseCase: GetPopularMoviesUseCase
+    private val getPopularMoviesUseCase: GetPopularMoviesUseCase,
+    private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
     init {
@@ -26,7 +27,7 @@ class MoviesViewModel @Inject constructor(
         language: String? = null,
         page: Int? = null,
         region: String? = null
-    ) = viewModelScope.launch(Dispatchers.IO) {
+    ) = viewModelScope.launch(dispatcher) {
         popularMovies.apply {
             postValue(Resource.Loading())
             postValue(getPopularMoviesUseCase.execute(language, page, region))
